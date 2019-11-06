943bb6f6cd09a590cb916ded93226dcb872e276c
Francesco  Aloisio <faloisio@mac-3017.local>
2019-11-06 12:45:11 +0000

    First version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Cartrawler React experiments',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learning how to use react </h1>
+    <p>This is a paragraph</p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
 
 export default IndexPage
+=
